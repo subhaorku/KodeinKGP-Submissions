f0bd8372d75044a36b83e66a19320514af075326
Subhasini Gupta <guptasubhasini68@gmail.com>
2023-07-10 02:11:20 +0530

    week-1 submissions

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("demobutton");
+
+button.addEventListener("click", function(){
+    // Color change effect
+    button.style.backgroundColor = "#ADD8E6";
+    button.style.color = "black";
+    alert("Wow! Hey You clicked the button!");
+})
